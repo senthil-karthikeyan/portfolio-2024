d277fb705acc2f85160014be5d927cc6090e7fc9
senthil <senthil.k@makoitlab.com>
2025-12-03 02:52:31 +0530

    refactor: Remove OrbitControls import from Scene component

--- components/Scene.tsx
+++ components/Scene.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { Canvas } from "@react-three/fiber";
 import Model from "@/components/model/Hero";
-import { Environment, OrbitControls } from "@react-three/drei";
+import { Environment } from "@react-three/drei";
 
 export default function CanvaScene() {
   return (
