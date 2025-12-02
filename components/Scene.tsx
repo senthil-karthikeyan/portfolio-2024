90096ac51dff8a95d485a70b04b58f1d153ef218
senthil <senthil.k@makoitlab.com>
2025-12-03 03:03:03 +0530

    refactor: Remove OrbitControls from Scene component to simplify the scene rendering

--- components/Scene.tsx
+++ components/Scene.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { Canvas } from "@react-three/fiber";
 import Model from "@/components/model/Hero";
-import { Environment ,OrbitControls} from "@react-three/drei";
+import { Environment } from "@react-three/drei";
 
 export default function CanvaScene() {
   return (
@@ -9,7 +9,6 @@ export default function CanvaScene() {
       <Model />
       <directionalLight intensity={2} position={[0, 2, 3]} />
       <Environment preset="city" />
-      <OrbitControls/>
     </Canvas>
   );
 }
